226971d474bd60946aff8bf0ff4f07977ec650e7
Maycon Felipe Mota <mayconfelipemotamw3@gmail.com>
2022-11-17 22:29:38 -0300

    fix: ajustes no crawler para burlar o cloudflare

--- airflow2/dags/tasks/extract.py
+++ airflow2/dags/tasks/extract.py
@@ -92,39 +92,43 @@ def extract_shopcar_page(pagina):
     import pandas 
     import json 
     import requests
+    import cloudscraper
     from bs4 import BeautifulSoup
-    # import cloudscraper
-    from selenium import webdriver
-    from selenium import webdriver
-    from selenium.webdriver.chrome.service import Service as ChromeService
-    from webdriver_manager.chrome import ChromeDriverManager
+
     # from fake_useragent import UserAgent
 
     # ua = UserAgent(verify_ssl=False)
     # userAgent = ua.random
-    chrome_options = webdriver.ChromeOptions()
-    chrome_options.add_argument("start-maximized")
-    chrome_options.add_argument('--no-sandbox')
-    chrome_options.add_argument('--window-size=1920,1080')
-    chrome_options.add_argument('--headless')
-    chrome_options.add_argument('--disable-gpu')
-    chrome_options.add_argument(f'user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/107.0.0.0 Safari/537.36')
-    chrome_options.add_argument("--disable-blink-features=AutomationControlled")
-    chrome_options.add_argument('--disable-dev-shm-usage')        
-
 
     print(f"Iniciando scrapping ...... {pagina}")
     url = f"https://www.shopcar.com.br/busca.php?tipo=1&marca=&string=&ordenar=valor_asc&pagina={pagina}"
-    driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()), chrome_options=chrome_options)
-
-    driver.get(url)
-
-    res = driver.page_source
-    # print(res)
-
+    scraper = cloudscraper.create_scraper(browser={'browser': 'firefox','platform': 'windows','mobile': False})
+    res = scraper.get(url).content
     soup = BeautifulSoup(res, "html.parser")
-    index = 0
     carro_dados = []
+
+    for carro in soup.find_all(class_='destaque-lista'):
+        try:
+            carro_modelo = carro('li')[1]('div')[0].get_text()
+            anoModelo_anoFabricacao = carro('li')[1]('div')[2].get_text()
+            corCarro = carro('li')[1]('div')[3].get_text()
+            combustivelCarro = carro('li')[1]('div')[4].get_text()
+            preco = carro(class_='preco')[0].get_text()
+            try:
+                kmCarro = carro(class_='caract-km')[0].get_text()
+            except:
+                kmCarro = 'Não disponível'
+            link_veiculo = carro('a')[1].get('href')
+            url = link_veiculo
+            res = requests.get(url)
+            soup = BeautifulSoup(res.text)
+            vendedor = soup.find_all(class_='dados1')[0].find_all(class_='nome')[0].get_text()
+            cidade = soup.find_all(class_='dados1')[0].find_all(class_='endereco')[0].get_text()
+            index += 1
+            carro_dados.append([carro_modelo, anoModelo_anoFabricacao, corCarro, combustivelCarro, kmCarro, preco, link_veiculo, vendedor, cidade])
+            print(f"Já foram incluídos {index} carros")
+        except:
+            pass
 
     for carro in soup.find_all(class_='itens'):
         carro_detalhes = carro.select(".coluna-2")
@@ -140,20 +144,19 @@ def extract_shopcar_page(pagina):
                 kmCarro = 'Não disponível'
             link_veiculo = carro('a')[1].get('href')
             url = link_veiculo
-            driver.get(url)
-            res = driver.page_source
-            soup = BeautifulSoup(res)
+            res = scraper.get(url).content
+            soup = BeautifulSoup(res, "html.parser")
             vendedor = soup.find_all(class_='dados1')[0].find_all(class_='nome')[0].get_text()
             cidade = soup.find_all(class_='dados1')[0].find_all(class_='endereco')[0].get_text()
             carro_dados.append([carro_modelo, anoModelo_anoFabricacao, corCarro, combustivelCarro, kmCarro, preco, link_veiculo, vendedor, cidade])
         except:
             pass
-        
+            print(carro_dados)
+
     carros_df = pandas.DataFrame(carro_dados)
-    print(carro_dados)
     carros_df.columns = ['Modelo', 'Ano', 'Cor',' Combustível','KM', 'Preco', 'Link', 'Vendedor', 'Cidade']
     carros_df.to_csv('./output_data/raw/shopcar/scrapping_anuncios_shopcar.csv', mode='a', header=False, encoding='utf8')
-        # if(pagina > 2):
-        #     carros_df.to_csv('./raw/scrapping_anuncios_shopcar.csv', mode='a', header=False, encoding='latin')
-        # else:
-        #     carros_df.to_csv('./raw/scrapping_anuncios_shopcar.csv', header=True,  encoding='latin')
+    if(pagina > 2):
+        carros_df.to_csv('./output_data/raw/shopcar/scrapping_anuncios_shopcar.csv', mode='a', header=False, encoding='utf8')
+    else:
+        carros_df.to_csv('./output_data/raw/shopcar/scrapping_anuncios_shopcar.csv', header=True,  encoding='utf8')
